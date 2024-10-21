c69174265cdb61ac1d86c87b81dee480f7b311f4
Dragon Warior <evelyn.gravett@protonmail.com>
2024-10-21 15:25:50 +0100

    add yaml library

--- core/src/main/java/group1/unisim/Main.java
+++ core/src/main/java/group1/unisim/Main.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import org.snakeyaml.engine.v2.api.*;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
