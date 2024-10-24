dab8b44d2bbb3ff118c3e0a3f50c39030b7547ae
Nestro-10 <oliver.barlow1256@gmail.com>
2024-10-24 11:23:06 +0100

    Created a simple GUI

--- core/src/main/java/group1/unisim/Main.java
+++ core/src/main/java/group1/unisim/Main.java
@@ -2,7 +2,9 @@ package group1.unisim;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -11,25 +13,39 @@ import org.snakeyaml.engine.v2.api.*;
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image;
+    private Texture toolbar;
+    private Texture mapTexture;
+    private Texture settingsTexture;
+    private Texture buildIconTexture;
+
 
     @Override
     public void create() {
+
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        toolbar = new Texture("toolbar.png");
+        mapTexture = new Texture("mapTexture.png");
+        settingsTexture = new Texture("settingsIcon.png");
+        buildIconTexture = new Texture("buildIcon.png");
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(mapTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight() );
+        batch.draw(toolbar, 0, 1420, Gdx.graphics.getWidth(), 80);
+        batch.draw(settingsTexture, 0, 1425, 70, 70);
+        batch.draw(buildIconTexture, 70, 1430, 60, 55 );
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        mapTexture.dispose();
+        toolbar.dispose();
+        settingsTexture.dispose();
+        buildIconTexture.dispose();
     }
 }
