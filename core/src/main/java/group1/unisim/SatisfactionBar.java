a02cc6e9026bcecf0a1df99353a4629bae62afd5
Dragon Warior <evelyn.gravett@protonmail.com>
2024-11-05 19:19:14 +0000

    set, get, remove thought

--- core/src/main/java/group1/unisim/SatisfactionBar.java
+++ core/src/main/java/group1/unisim/SatisfactionBar.java
@@ -1,16 +1,21 @@
 package group1.unisim;
 
 
-import static java.lang.Math.abs;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.Math.*;
 
 public class SatisfactionBar {
 
     private float satisfactionScore;
     private float target;
     private float speed = 0.03f;
+    private Map<String, Thought> thoughts;
 
     public SatisfactionBar(){
         resetScore();
+        thoughts = new HashMap<>();
     }
 
     private void resetScore(){
@@ -28,5 +33,29 @@ public class SatisfactionBar {
             float direction = (difference < 0) ? -1 : 1;
             satisfactionScore += speed * direction;
         }
+    }
+
+    public void setThought(String key, Thought thought){
+        thoughts.put(key, thought);
+        calculateTarget();
+    }
+
+    public Thought getThought(String key){
+        return thoughts.get(key);
+    }
+
+    public void removeThought(String key){
+        thoughts.remove(key);
+        calculateTarget();
+    }
+
+    private void calculateTarget(){
+        target = 50;
+
+        for (String key : thoughts.keySet()) {
+            target += thoughts.get(key).modification;
+        }
+
+        target = max(min(target, 0), 100);
     }
 }
